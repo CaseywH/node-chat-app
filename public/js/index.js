37bee42cae593c697d81d5b2bb815b6e38f058b5
Casey Heinemeyer <cheinemeyer@gmail.com>
2018-03-09 14:21:07 -0800

    Emit new message on create message

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,6 @@ var socket = io();
 socket.on('connect', function () {
   console.log('connected to server');
 
-  socket.emit('createMessage', {
-    from: 'joey bag-a-donuts',
-    text: 'hey this is Joey from East Tuna Fish'
-  });
-
 })
 
 socket.on('newMessage', function (message) {
